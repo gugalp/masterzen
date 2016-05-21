e35265b32f8b8564079232e371c821141db21e52
Guga <gugalp@gmail.com>
2016-05-21 01:11:07 -0300

    Start saving on Redis

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Node.JS!');
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+var Game = require("./models/game");
+
+var game = new Game();
+
+Game.methods.save(game);
--- models/game.js
+++ models/game.js
@@ -6,3 +6,52 @@ function Game(config) {
     this.players = [];
     this.sequence = "";
 };
+
+var redis = require("redis");
+var client;
+
+function initClient()
+{
+    client = redis.createClient();
+
+    client.on("error", function (err) {
+        console.log("Error " + err);
+    });
+}
+
+Game.methods = {
+    save: function(game)
+    {
+        initClient();
+        var gameHash = "game:" + game.gameId;
+        client.set(gameHash, game, redis.print);
+        client.quit();
+
+    },
+
+    load: function(gameId, callback)
+    {
+        initClient();
+        var gameHash = "game:" + game.gameId;
+        client.keys(gameHash, function(err, replies)
+            {
+                var game;
+
+                if (replies.length == 1)
+                {
+                    game = replies[0];
+                }
+                else
+                {
+                    err = "Not found!";
+                }
+
+                callback(err, game);
+                client.quit();
+            }
+        )
+    }
+};
+
+
+module.exports = Game;

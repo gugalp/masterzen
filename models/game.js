b2fefc613b326c0f3ce35dc0005e8eb6a2f123dc
Guga <gugalp@gmail.com>
2016-05-21 15:10:13 -0300

    Game stats for checking if players are eligible for another turn

--- models/game.js
+++ models/game.js
@@ -146,7 +146,13 @@ Game.prototype.guessCode = function(playerName, sequence)
 
 Game.prototype.stats = function()
 {
+    var ret = true;
+    this.players.forEach(function(player)
+    {
+        ret = ret && !player.turn;
+    });
 
+    return ret;
 }
 
 Game.prototype.getPlayer = function(playerName)

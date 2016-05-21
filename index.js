136d8e3d992ffa317eef15b4f1b6f28381c9f71f
Diogo Sgrillo <dsgrillo@live.com>
2016-05-21 13:38:57 -0300

    API definition

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+
+app.use(bodyParser.json());
+
+router.post('/NewGame', function(request, response) {
+
+    if(request.body.name == undefined) {
+        console.log('bad request');
+        response.send(400);
+        return;
+    } else {
+        console.log(request.body);
+
+        response.json({
+            'teste': 123
+        });
+    }
+});
+
+
+router.post('/Guess', function(request, response) {
+    
+});
+
+
+app.use('/API', router);
+
+app.listen(8080);
+
+
+/*
 var Game = require("./models/game");
 
 var game = new Game();
-
 game.gameId = "testeGame";
 game.sequence = "ABCD";
 Game.methods.save(game);
@@ -14,3 +48,4 @@ Game.methods.load(
         console.log(JSON.stringify(ret));
     }
 );
+*/
